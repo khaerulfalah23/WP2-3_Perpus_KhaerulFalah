0fb0ab0ab419b929bb03c94c75526bd0cfc8a707
khaerulfalah23 <khaerulfalah23@gmail.com>
2023-11-15 19:03:33 +0700

    Tugas p11 (Membuat CRUD buku)

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -71,7 +71,7 @@ class ModelBuku extends CI_Model
         //$this->db->select('buku.id_kategori,kategori.kategori');
         $this->db->select('*');
         $this->db->from('buku');
-        $this->db->join('kategori','kategori.id = buku.id_kategori');
+        $this->db->join('kategori','kategori.id_kategori = buku.id_kategori');
         $this->db->where($where);
         return $this->db->get();
     }
---
+++ application/views/buku/ubah_buku.php
@@ -0,0 +1,84 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <?= $this->session->flashdata('pesan'); ?>
+    <div class="row">
+        <div class="col-lg-6">
+            <?php if(validation_errors()){?>
+            <div class="alert alert-danger" role="alert">
+                <?= validation_errors();?>
+            </div>
+            <?php }?>
+            <?= $this->session->flashdata('pesan'); ?>
+
+
+            <!-- KOnten -->
+            <form action="<?= base_url('buku/ubahbuku/'. $buku['id']); ?>" method="post" enctype="multipart/form-data">
+                <div class='row'>
+                    <div class="col-sm-3">
+                        <img src="<?= base_url('assets/img/upload/') . $buku['image']; ?>" class="img-thumbnail" alt="">
+                    </div>
+                    <div class='col-sm-9'>
+                        <div class="modal-body">
+                            <input type="hidden" class="form-control form-control-user" id="id"
+                                name="id" value="<?= $buku['id']; ?>">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="judul_buku"
+                                    name="judul_buku" placeholder="Masukkan Judul Buku"
+                                    value="<?= $buku['judul_buku']; ?>">
+                            </div>
+                            <div class="form-group">
+                                <select name="id_kategori" class="form-control form-control-user">
+                                    <?php foreach ($kategori as $k) { ?>
+                                        <?php if($k['id_kategori'] == $buku['id_kategori']): ?>
+                                            <option value="<?= $k['id_kategori'];?>" selected><?= $k['nama_kategori'];?></option>
+                                        <?php else: ?> 
+                                            <option value="<?= $k['id_kategori'];?>"><?= $k['nama_kategori'];?></option>
+                                        <?php endif; ?>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="pengarang"
+                                    name="pengarang" placeholder="Masukkan nama pengarang" value='<?= $buku['pengarang']; ?>'>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="penerbit" name="penerbit" placeholder="Masukkan nama penerbit" value='<?= $buku['penerbit']; ?>'>
+                            </div>
+                            <div class="form-group">
+                                <select name="tahun" class="form-control form-control-user">
+                                    <option value="<?= $buku['tahun_terbit']; ?>"><?= $buku['tahun_terbit']; ?></option>
+                                    <?php for ($i=date('Y'); $i > 1000 ; $i--) { ?>
+                                    <option value="<?= $i;?>"><?= $i;?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="isbn" name="isbn"
+                                    placeholder="Masukkan ISBN" value='<?= $buku['isbn']; ?>'>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="stok" name="stok"
+                                    placeholder="Masukkan nominal stok" value='<?= $buku['stok']; ?>'>
+                            </div>
+                            <div class="form-group">
+                                <input type="file" class="form-control form-control-user" id="image" name="image">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" onclick="window.history.back(-1)"><i
+                                    class="fas fa-ban"></i>Close</button>
+                            <button type="submit" class="btn btn-primary"><i class="fas fa-plus-circle"></i>
+                                Update</button>
+                        </div>
+                    </div>
+                </div>
+
+            </form>
+
+
+        </div>
+    </div>
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- End of Main Content -->
